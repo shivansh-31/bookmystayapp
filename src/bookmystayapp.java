f26f4b1e28c2cd2a1f7639b2eb76fc507120a301
shivansh-31 <shivansh.31m@gmail.com>
2026-03-16 09:41:30 +0530

    UC1:

--- src/bookmystayapp.java
+++ src/bookmystayapp.java
@@ -1,2 +1,15 @@
-public class bookmystayapp {
+/**
+ * Book My Stay App
+ * Hotel Booking Management System
+ * @author shivansh dhingra
+ * @version 1.0
+ */
+
+public class BookMyStayApp {
+    public static void main(String[] args) {
+        System.out.println("Book My Stay");
+        System.out.println("Hotel Booking Management System");
+        System.out.println("Version 1.0");
+        System.out.println("Welcome to the Application");
+    }
 }
